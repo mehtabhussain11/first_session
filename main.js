ee94d6d3c7e52c9fc7def77901f0ed8109e77494
mehtabhussain11 <161445142+mehtabhussain11@users.noreply.github.com>
2024-03-28 11:15:05 +0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,2 @@
-console.log('hello world');
+var personName = "Mehtab Hussain";
+console.log("hello", personName, "would you like to learn some python today?");
--- main.ts
+++ main.ts
@@ -1,1 +1,2 @@
-console.log('hello world');
+let personName = "Mehtab Hussain";
+console.log("hello",personName, "would you like to learn some python today?");
